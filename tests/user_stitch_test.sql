8601100a0bc05e3e522e66ccc3d9a62543d87e1f
Ubuntu <ubuntu@ip-172-31-26-242.us-east-2.compute.internal>
2022-07-06 05:25:51 +0000

    test to confirm # of records with customer ids in source match the model count

--- tests/user_stitch_test.sql
+++
@@ -1,7 +0,0 @@
---testing for no customer ids with more than one visitor id
-
-select customer_id, 
-from {{ref('users_stitched')}}
-where customer_id is not null
-group by 1 
-having count(distinct visitor_id) > 1
